9a6616b6eaca46bd4416bd7cad493b2b28ae159e
Ayaka Arai <spr2140038@stu.o-hara.ac.jp>
2023-02-02 13:56:45 +0900

    ログイン実装

--- lib/footer.dart
+++ lib/footer.dart
@@ -1,10 +1,33 @@
-import 'package:fi_ma/home_screen.dart';
+import 'package:fi_ma/login.dart';
+import 'package:fi_ma/view/home_screen.dart';
 import 'package:fi_ma/view/budget/budget.dart';
 import 'package:fi_ma/view/deferred_payment/deferred_payment_list.dart';
 import 'package:fi_ma/view/register/exin_list.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:fi_ma/view/menu.dart';
 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+    title: 'Flutter app',
+    home: StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          // スプラッシュ画面などに書き換えても良い
+          return const SizedBox();
+        }
+        if (snapshot.hasData) {
+          // User が null でなない、つまりサインイン済みのホーム画面へ
+          return const Footer();
+        }
+        // User が null である、つまり未サインインのサインイン画面へ
+        return LoginScreen();
+      },
+    ),
+  );
+}
 
 class Footer extends StatefulWidget {
   const Footer({Key? key}) : super(key: key);
--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,76 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'footer.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+class googlSignInNotifier extends ChangeNotifier {
+  final _googleSignIn = GoogleSignIn();
+  final _auth = FirebaseAuth.instance;
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  GoogleSignInAccount? _user;
+  GoogleSignInAccount get user => _user!;
+  FirebaseAuth get auth => _auth;
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.orange,
-      ),
-      localizationsDelegates: [
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-      ],
-      routes: <String, WidgetBuilder>{
-        '/': (_) => Footer(),
-      },
-    );
+  Future googleLogin() async {
+    try {
+      final googleUser = await _googleSignIn.signIn();
+
+      if (googleUser == null) return;
+      _user = googleUser;
+
+      final GoogleSignInAuthentication googlAuth =
+      await googleUser.authentication;
+
+      final credential = GoogleAuthProvider.credential(
+        accessToken: googlAuth.accessToken,
+        idToken: googlAuth.idToken,
+      );
+
+      await _auth.signInWithCredential(credential);
+      notifyListeners();
+    } catch (e) {
+      print('Error: $e');
+    }
+  }
+
+  Future logout() async {
+    await _googleSignIn.signOut();
+    FirebaseAuth.instance.signOut();
   }
 }
+
+final googlSignInProvider = ChangeNotifierProvider((ref) {
+  return googlSignInNotifier();
+});
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  runApp(ProviderScope(child: MyApp()));
+}
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+//
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter Demo',
+//       theme: ThemeData(
+//         primarySwatch: Colors.orange,
+//       ),
+//       localizationsDelegates: [
+//         GlobalMaterialLocalizations.delegate,
+//         GlobalWidgetsLocalizations.delegate,
+//       ],
+//       routes: <String, WidgetBuilder>{
+//         '/': (_) => Footer(),
+//       },
+//     );
+//   }
+// }
